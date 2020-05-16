47792c5dc17e34ae66ec3add6b09398bd3ab4349
Daniel Pôrto Núñez <daniel@MacBook-Pro-de-Daniel.local>
2020-05-16 14:58:45 -0300

    Updates

---
+++ precache-manifest.db92d1c764d012fde32ade854415d9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fe4390183e73c9349c96866eb94497",
+    "url": "/book-swing/index.html"
+  },
+  {
+    "revision": "a58298cc32a6048507c5",
+    "url": "/book-swing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6dade5802d93cdd2f7e",
+    "url": "/book-swing/static/js/2.8820cfd7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/book-swing/static/js/2.8820cfd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58298cc32a6048507c5",
+    "url": "/book-swing/static/js/main.3c1eda86.chunk.js"
+  },
+  {
+    "revision": "a9b1b85d06bbf987432b",
+    "url": "/book-swing/static/js/runtime-main.2dc57563.js"
+  }
+]);
