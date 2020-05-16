e1db1700ab34ab4d5720f622167a7380822b5c9d
Daniel Pôrto Núñez <daniel@MacBook-Pro-de-Daniel.local>
2020-05-16 14:01:26 -0300

    style (book's side): fix box shadow

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -28,14 +28,13 @@ const Book = () => {
 	)
 
 	return (
-		<div style={{width: '100%', height: '80%', display: 'flex', flexDirection: 'column'}}>
-			<div style={{width: '100%', height: '100%' , display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+		<>
 				<IconButton disabled={store.page === 0} onClick={previousPage}>
 					<KeyboardArrowLeftIcon fontSize='large'/>
 				</IconButton>
 				<BookContainer closed={(store.page === 0 && 'start') || (store.page === store.total && 'end')}>
-					<Side paginated />
-					<Side>
+					<Side paginated closed={store.page === 0} />
+					<Side closed={store.page === store.total}>
 						<Page front={4} back={5} bg='blue'/>
 						<Page front={2} back={3} bg='green'/>
 						<Page front={0} back={1} bg='grey'/>
@@ -44,11 +43,7 @@ const Book = () => {
 				<IconButton disabled={store.page === store.total} onClick={nextPage}>
 					<KeyboardArrowRightIcon fontSize='large' />
 				</IconButton>
-			</div>
-			<div style={{display: 'flex', justifyContent: 'center'}}>
-				<div style={{boxShadow: '0px 117px 110px -93px rgba(0,0,0,0.75)', transition: 'max-width 0.4s ease 0.4s', width: '100%', maxWidth: ((store.page === 0 && 'start') || (store.page === store.total && 'end')) ? '33%' : '71%'}}>sauhshua</div>
-			</div>
-		</div>
+		</>
 	)
 }
 
@@ -56,17 +51,18 @@ const Book = () => {
 const BookContainer = styled.div`
 	display: flex;
 	position: relative;
-	height: 100%;
+	height: 80%;
 	width: 70%;
 	transform: ${({closed}) => `translateX(${closed ? (closed === 'start' ? '-24%' : '24%') : '0'})`};
 	transition: transform 0.5s ease 0.4s;
 `
 
 const Side = styled.div`
+	box-shadow: ${({closed}) => `0 10px 20px rgba(0,0,0, ${closed ? '0' : '0.19'}), 0 6px 6px rgba(0,0,0, ${closed ? '0' : '0.23'})`};
 	flex: 1;
 	perspective: 1500px;
 	perspective-origin: calc(50% + 50px) 50%;
-	transition: box-shadow ease 0.2s;
+	${({closed}) => closed ? '' : 'transition: box-shadow ease 0.4s 0.5s'};
 `
 
 export default Book
